55560e695c037598ebe6fd91f7f6260535880808
dsladovi <dsladovi@foi.hr>
2014-04-01 11:28:58 +0200

    pop
    kdsds

--- aplikacijaLAB/Form1.Designer.cs
+++ aplikacijaLAB/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace aplikacijaLAB
 {
-    partial class Form1
+    partial class FrmGlavna
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnIzracunaj = new System.Windows.Forms.Button();
+            this.btnOcisti = new System.Windows.Forms.Button();
+            this.txtPolumjer = new System.Windows.Forms.TextBox();
+            this.txtPovrsina = new System.Windows.Forms.TextBox();
+            this.txtOpseg = new System.Windows.Forms.TextBox();
+            this.lblPolumjer = new System.Windows.Forms.Label();
+            this.lblPovrsina = new System.Windows.Forms.Label();
+            this.lblOpseg = new System.Windows.Forms.Label();
+            this.lstRezultati = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnIzracunaj
+            // 
+            this.btnIzracunaj.Location = new System.Drawing.Point(186, 160);
+            this.btnIzracunaj.Name = "btnIzracunaj";
+            this.btnIzracunaj.Size = new System.Drawing.Size(75, 23);
+            this.btnIzracunaj.TabIndex = 0;
+            this.btnIzracunaj.Text = "Izracunaj";
+            this.btnIzracunaj.UseVisualStyleBackColor = true;
+            this.btnIzracunaj.Click += new System.EventHandler(this.btnIzracunaj_Click);
+            // 
+            // btnOcisti
+            // 
+            this.btnOcisti.Location = new System.Drawing.Point(186, 189);
+            this.btnOcisti.Name = "btnOcisti";
+            this.btnOcisti.Size = new System.Drawing.Size(75, 23);
+            this.btnOcisti.TabIndex = 1;
+            this.btnOcisti.Text = "Ocisti";
+            this.btnOcisti.UseVisualStyleBackColor = true;
+            // 
+            // txtPolumjer
+            // 
+            this.txtPolumjer.Location = new System.Drawing.Point(66, 49);
+            this.txtPolumjer.Name = "txtPolumjer";
+            this.txtPolumjer.Size = new System.Drawing.Size(96, 20);
+            this.txtPolumjer.TabIndex = 2;
+            this.txtPolumjer.Text = "0,00";
+            this.txtPolumjer.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtPovrsina
+            // 
+            this.txtPovrsina.Location = new System.Drawing.Point(66, 89);
+            this.txtPovrsina.Name = "txtPovrsina";
+            this.txtPovrsina.Size = new System.Drawing.Size(99, 20);
+            this.txtPovrsina.TabIndex = 3;
+            this.txtPovrsina.Text = "0,00";
+            this.txtPovrsina.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtOpseg
+            // 
+            this.txtOpseg.Location = new System.Drawing.Point(66, 128);
+            this.txtOpseg.Name = "txtOpseg";
+            this.txtOpseg.Size = new System.Drawing.Size(96, 20);
+            this.txtOpseg.TabIndex = 4;
+            this.txtOpseg.Text = "0,00";
+            this.txtOpseg.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblPolumjer
+            // 
+            this.lblPolumjer.AutoSize = true;
+            this.lblPolumjer.Location = new System.Drawing.Point(13, 52);
+            this.lblPolumjer.Name = "lblPolumjer";
+            this.lblPolumjer.Size = new System.Drawing.Size(50, 13);
+            this.lblPolumjer.TabIndex = 5;
+            this.lblPolumjer.Text = "Polumjer:";
+            // 
+            // lblPovrsina
+            // 
+            this.lblPovrsina.AutoSize = true;
+            this.lblPovrsina.Location = new System.Drawing.Point(12, 92);
+            this.lblPovrsina.Name = "lblPovrsina";
+            this.lblPovrsina.Size = new System.Drawing.Size(51, 13);
+            this.lblPovrsina.TabIndex = 6;
+            this.lblPovrsina.Text = "Povrsina:";
+            // 
+            // lblOpseg
+            // 
+            this.lblOpseg.AutoSize = true;
+            this.lblOpseg.Location = new System.Drawing.Point(12, 131);
+            this.lblOpseg.Name = "lblOpseg";
+            this.lblOpseg.Size = new System.Drawing.Size(41, 13);
+            this.lblOpseg.TabIndex = 7;
+            this.lblOpseg.Text = "Opseg:";
+            // 
+            // lstRezultati
+            // 
+            this.lstRezultati.FormattingEnabled = true;
+            this.lstRezultati.Location = new System.Drawing.Point(168, 49);
+            this.lstRezultati.Name = "lstRezultati";
+            this.lstRezultati.Size = new System.Drawing.Size(104, 95);
+            this.lstRezultati.TabIndex = 8;
+            this.lstRezultati.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstRezultati_MouseDoubleClick);
+            // 
+            // FrmGlavna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 212);
+            this.Controls.Add(this.lstRezultati);
+            this.Controls.Add(this.lblOpseg);
+            this.Controls.Add(this.lblPovrsina);
+            this.Controls.Add(this.lblPolumjer);
+            this.Controls.Add(this.txtOpseg);
+            this.Controls.Add(this.txtPovrsina);
+            this.Controls.Add(this.txtPolumjer);
+            this.Controls.Add(this.btnOcisti);
+            this.Controls.Add(this.btnIzracunaj);
+            this.Name = "FrmGlavna";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Froma glavne aplikacije";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnIzracunaj;
+        private System.Windows.Forms.Button btnOcisti;
+        private System.Windows.Forms.TextBox txtPolumjer;
+        private System.Windows.Forms.TextBox txtPovrsina;
+        private System.Windows.Forms.TextBox txtOpseg;
+        private System.Windows.Forms.Label lblPolumjer;
+        private System.Windows.Forms.Label lblPovrsina;
+        private System.Windows.Forms.Label lblOpseg;
+        private System.Windows.Forms.ListBox lstRezultati;
     }
 }
 
--- aplikacijaLAB/Form1.cs
+++ aplikacijaLAB/Form1.cs
@@ -10,11 +10,50 @@ using System.Windows.Forms;
 
 namespace aplikacijaLAB
 {
-    public partial class Form1 : Form
+    public partial class FrmGlavna : Form
     {
-        public Form1()
+        public FrmGlavna()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIzracunaj_Click(object sender, EventArgs e)
+        {
+            float polumjer = 0;
+            float povrsina = 0;
+            float opseg = 0;
+            float.TryParse(txtPolumjer.Text, out polumjer);
+            Izracunaj(polumjer, ref povrsina, ref opseg);
+        }
+
+        private void Izracunaj(float polumjer, ref float povrsina, ref float opseg)
+        {
+            if (polumjer > 0)
+            {
+                povrsina = polumjer * polumjer * (float)Math.PI;
+                opseg = 2 * polumjer * (float)Math.PI;
+
+                txtPovrsina.Text = povrsina.ToString();
+                txtOpseg.Text = opseg.ToString();
+
+            }
+        }
+        /// <summary>
+       ///ova metoda sluzi za ispis rez
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lstRezultati_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            float polumjer = 0;
+            float.TryParse(lstRezultati.SelectedItem.ToString(),out polumjer);
+            txtPolumjer.Text = polumjer.ToString();
+            btnIzracunaj_Click(null,null);
         }
     }
 }
